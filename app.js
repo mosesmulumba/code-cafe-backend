aebb34447ce93d71f0da8aac3f0b021bf5a3e0a7
mosesmulumba <mulumbamoses94@gmail.com>
2023-06-22 14:26:15 +0300

    added the cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const config = require('./config');
 const routes = require('./routes');
@@ -20,12 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.setHeader('Acess-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hi!' });
